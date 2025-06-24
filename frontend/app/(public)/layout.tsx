0475fa3993c4133686d928a19548ac6a29daf4e0
dhruvishavaghani <dhruvishavaghani0509@gmail.com>
2025-06-24 17:37:58 +0530

    fix: diagram issues

--- frontend/app/(public)/layout.tsx
+++ frontend/app/(public)/layout.tsx
@@ -1,6 +1,10 @@
 import { Toaster } from "sonner";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+
+  
+  console.log(process.env.AUTH_SECRET)
+
   return (
     <div className="flex h-screen w-full">
       <main className="flex-1 overflow-auto">

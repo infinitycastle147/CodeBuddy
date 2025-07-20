31a250b34432e489b3206e6accc3ddc2d4c05237
dhruvishavaghani <dhruvishavaghani0509@gmail.com>
2025-07-20 18:46:37 +0530

    fix: remove unused parameter in session route

--- frontend/app/api/auth/session/route.ts
+++ frontend/app/api/auth/session/route.ts
@@ -1,8 +1,8 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
     const session = await getServerSession(authOptions);
     
